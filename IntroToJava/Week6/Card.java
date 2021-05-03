c16fcbb6c8389a7c5760dd809653887c9e785939
Brandon <47669645+BrandoTheDev@users.noreply.github.com>
2021-05-03 14:56:58 -0500

    Update Card.java

--- IntroToJava/Week6/Card.java
+++ IntroToJava/Week6/Card.java
@@ -1,23 +1,28 @@
-
 public class Card {
-	
-	private int value;
-	private String name;
-	
-	public Card(String name, int value) {
-		this.name = name;
-		this.value = value;
-	}
-	
-	public int getValue() {
-		return value;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void describe(Card card) {
-		System.out.println(card.getName() + " -> " + card.getValue());
-	}
+
+    private int value;
+    private String name;
+    private String suit;
+
+    public Card(String name, String suit, int value) {
+	this.suit = suit;
+	this.name = name;
+	this.value = value;
+    }
+
+    public int getValue() {
+	return value;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public String getSuit() {
+	return suit;
+    }
+
+    public void describe(Card card) {
+	System.out.println(card.getName() + card.getSuit() + " = " + card.getValue());
+    }
 }
