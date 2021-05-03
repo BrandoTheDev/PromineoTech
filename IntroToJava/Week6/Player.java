760bbd90d7530bd2ff8b9a04f9e7ef3fd63d5fbe
Brandon <47669645+BrandoTheDev@users.noreply.github.com>
2021-05-03 14:57:57 -0500

    Update Player.java

--- IntroToJava/Week6/Player.java
+++ IntroToJava/Week6/Player.java
@@ -2,52 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-	
-	private List<Card> hand = new ArrayList<Card>();
-	private int score;
-	private String name;
-	
-	public Player(String name) {
-		this.name = name;
-		score = 0;
-	}
-	
-	public void describe() 
-	{
-		if (hand.size() >= 0) {
-			System.out.println(name + " has " + hand.get(hand.size()-1).getName());
-		}
-	}
-	
-	public int flip(Card flipCard)
-	{
-		return flipCard.getValue();
-	}
-	
-	public Card flipCard() {
-		Card topCard = hand.remove(hand.size()-1);
-		return topCard;
-	}
-	
-	public void draw(Deck deck)
-	{
-		hand.add(deck.draw());
-	}
-	
-	public void incrementScore() {
-		score += 1;
-	}
 
-	public int getScore() {
-		return score;
-	}
+    private List<Card> hand = new ArrayList<Card>();
+    private int score;
+    private String name;
 
-	public String getName() {
-		return name;
-	}
-	
-	public String fancyScore() {
-		return name + " has a score of " + score;
-	}
+    public Player(String name) {
+	this.name = name;
+	score = 0;
+    }
 
+    public void describe() {
+	if (hand.size() > 0) {
+	    Card currentCard = hand.get(hand.size() - 1);
+	    System.out.println(name + " has " + currentCard.getName() + currentCard.getSuit());
+	}
+    }
+
+    public int flip(Card flipCard) {
+	return flipCard.getValue();
+    }
+
+    public Card flipCard() {
+	Card topCard = hand.remove(hand.size() - 1);
+	return topCard;
+    }
+
+    public void draw(Deck deck) {
+	hand.add(deck.draw());
+    }
+
+    public void incrementScore() {
+	score += 1;
+    }
+
+    public int getScore() {
+	return score;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public String fancyScore() {
+	return name + " has a score of " + score;
+    }
 }
